bf3a7c9a33a807b9eab879410243d526a07b96b0
json99 <branson0419@gmail.com>
2025-10-15 13:25:54 -0700

    stop tracking databseConnection

--- controllers/databaseConnection.js
+++
@@ -1,4 +0,0 @@
-// controllers/databaseConnection.js
-module.exports = {
-    uri: 'mongodb+srv://haoran:041933@workshop-bakery.szricxr.mongodb.net/?retryWrites=true&w=majority&appName=Workshop-Bakery'
-};
